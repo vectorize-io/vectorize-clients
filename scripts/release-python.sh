e461854e6ed0039ef268c597a7c2c499aed7312a
Nicolò Boschi <boschi1997@gmail.com>
2025-02-20 12:04:27 +0100

    fix version format

--- scripts/release-python.sh
+++ scripts/release-python.sh
@@ -5,6 +5,10 @@ if [[ -z "$version" ]]; then
   echo "Usage: release-python.sh <version>"
   exit 1
 fi
+if [[ ! $version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "Version must be in the format x.y.z"
+  exit 1
+fi
 ROOT_DIR=$(git rev-parse --show-toplevel)
 SRC_DIR=$ROOT_DIR/src
 npm run edit-toml $SRC_DIR/python/pyproject.toml tool.poetry version $version
