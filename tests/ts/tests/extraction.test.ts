8aaef7c0591f77527587c216102039ab86c9a5f5
Nicolò Boschi <boschi1997@gmail.com>
2025-03-05 09:42:26 +0100

    feat: generate with /files api (#3)

--- tests/ts/tests/extraction.test.ts
+++ tests/ts/tests/extraction.test.ts
@@ -1,6 +1,7 @@
 import {beforeEach, describe, it, expect} from "vitest";
 import {createTestContext, TestContext} from "./testContext";
-import {ExtractionApi} from "@vectorize-io/vectorize-client";
+import {ExtractionApi, FilesApi} from "@vectorize-io/vectorize-client";
+import * as fs from "node:fs";
 
 export let testContext: TestContext;
 
@@ -12,13 +13,33 @@ describe("extraction", () => {
     it("verify iris extraction", async () => {
         let extractionApi = new ExtractionApi(testContext.configuration)
         try {
-            const text = "# Hello\n\n## This is a subtitle\n\nThis is a paragraph\n\n- item 1\n- item 2\n\n";
-            const blob = new Blob([text], { type: "text/markdown" });
+            const startResponse = await new FilesApi(testContext.configuration)
+                .startFileUpload({
+                    organization: testContext.orgId,
+                    startFileUploadRequest: {
+                        name: "test",
+                        contentType: "application/pdf"
+                    }
+                })
+
+            const fileBuffer = fs.readFileSync("tests/research.pdf");
+            const fetchResponse = await fetch(startResponse.uploadUrl, {
+                method: 'PUT',
+                body: fileBuffer,
+                headers: {
+                    'Content-Type': 'application/pdf'
+                },
+            });
+            if (!fetchResponse.ok) {
+                throw new Error(`Failed to upload file: ${fetchResponse.statusText}`);
+            }
 
             const response = await extractionApi.startExtraction({
                 organization: testContext.orgId,
-                file: blob,
-                chunkSize: 512,
+                startExtractionRequest: {
+                    fileId: startResponse.fileId,
+                    chunkSize: 512,
+                }
             })
             await pollExtraction(extractionApi, response.extractionId)
 
@@ -32,20 +53,20 @@ describe("extraction", () => {
 
     async function pollExtraction(extractionApi: ExtractionApi, extractionId: string) {
         while (true) {
-                const result = await extractionApi.getExtractionResult({
-                    organization: testContext.orgId,
-                    extractionId: extractionId
-                })
-                if (result.ready) {
-                    expect(result.data?.success).toBe(true)
-                    expect(result.data?.error).toBeFalsy()
-                    expect(result.data?.chunks?.length).toBe(1)
-                    expect(result.data?.text).toBeTruthy()
-                    break
-                } else {
-                    console.log("not ready")
-                }
+            const result = await extractionApi.getExtractionResult({
+                organization: testContext.orgId,
+                extractionId: extractionId
+            })
+            if (result.ready) {
+                expect(result.data?.success).toBe(true)
+                expect(result.data?.error).toBeFalsy()
+                expect(result.data?.chunks?.length).toBe(3)
+                expect(result.data?.text).toBeTruthy()
+                break
+            } else {
+                console.log("not ready")
             }
+        }
     }
 
 });
