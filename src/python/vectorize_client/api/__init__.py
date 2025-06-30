18ea70c09a5610754bad1d55176dc8612c486ed4
Ben <ben.bartholomew@vectorize.io>
2025-06-30 16:25:13 -0400

    add avanced to the spec

--- src/python/vectorize_client/api/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from vectorize_client.api.connectors_api import ConnectorsApi
-from vectorize_client.api.extraction_api import ExtractionApi
-from vectorize_client.api.files_api import FilesApi
-from vectorize_client.api.pipelines_api import PipelinesApi
-from vectorize_client.api.uploads_api import UploadsApi
-
